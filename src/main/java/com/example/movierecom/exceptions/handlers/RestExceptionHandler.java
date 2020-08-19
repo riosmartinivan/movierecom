d7cef6e14779cfee24164e230b672c367a13e8ac
Martin Ivan Rios <rios.martinivan@gmail.com>
2020-08-19 19:13:34 -0300

    Added another exception handler to manage when you have an invalid API Key

--- src/main/java/com/example/movierecom/exceptions/handlers/RestExceptionHandler.java
+++ src/main/java/com/example/movierecom/exceptions/handlers/RestExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -14,11 +15,20 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
-    protected ResponseEntity<Object> handleBadRequestException(RuntimeException ex) {
+    protected ResponseEntity<Object> handleBadRequestException(BadRequestException ex) {
         ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage());
         return buildResponseEntity(apiError);
     }
 
+    @ExceptionHandler(HttpClientErrorException.class)
+    protected ResponseEntity<Object> handleHttpClientErrorException(HttpClientErrorException ex) {
+        String message = ex.getMessage();
+        if (ex.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+            message = "Unauthorized: Invalid API key";
+        }
+        ApiError apiError = new ApiError(ex.getStatusCode(), message);
+        return buildResponseEntity(apiError);
+    }
 
     private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
         return new ResponseEntity<>(apiError, apiError.getStatus());
